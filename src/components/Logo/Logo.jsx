7543a628134f9d0bb0455c9331b93aee183a1e53
Artsiom Zhalkouski <azhalkouski@gmail.com>
2018-11-27 21:05:28 +0300

    Implement messenger page layout

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,9 +1,17 @@
+/* eslint react/require-default-props: 0 */
 import React from 'react';
+import PropTypes from 'prop-types';
 import './styles.css';
 
-export default () => (
-  <div className="logo">
+const Logo = ({ theme = 'dark' }) => (
+  <div className={`logo logo--${theme}`}>
     <div className="logo__icon" />
     <span className="logo__text">Messenger</span>
   </div>
 );
+
+Logo.propTypes = {
+  theme: PropTypes.string,
+};
+
+export default Logo;
