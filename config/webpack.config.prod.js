d0db739edf1d697456ebaa09a7f4dfe4a2645389
Artsiom Zhalkouski <azhalkouski@gmail.com>
2018-11-30 15:20:00 +0300

    Add postcss, style linting, style optimisation for broduction build

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,9 +1,91 @@
-const merge = require('webpack-merge');
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const common = require('./webpack.config.common.js');
+const dir = {
+  modules: resolve(__dirname, '..', 'node_modules'),
+  config: resolve(__dirname),
+};
 
-
-module.exports = merge(common, {
+module.exports = {
 
   mode: 'production',
-});
+
+  entry: {
+    index: './src/index.jsx',
+  },
+
+  output: {
+    filename: 'bundle.js',
+    path: resolve(__dirname, '..', 'build'),
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        exclude: dir.modules,
+        loader: 'eslint-loader',
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: dir.modules,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+          },
+          {
+            // collect CSS from all the css files referenced in the app and
+            // put it into a string
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              config: {
+                path: dir.config,
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+    ],
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: 'public/index.html',
+      title: 'Messenger',
+    }),
+  ],
+};
