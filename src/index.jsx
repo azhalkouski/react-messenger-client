b0c6acbf2aa06081e18533ed7d6f05abe63fafb3
Artsiom Zhalkouski <azhalkouski@gmail.com>
2018-12-11 18:57:56 +0300

    Implement Auth

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
-import BrowserRouter from 'react-router-dom/BrowserRouter';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import Router from 'react-router-dom/Router';
 import Route from 'react-router-dom/Route';
-import '@babel/polyfill';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import '@babel/polyfill';
+import history from './utils/history';
+import reducers from './reducers';
 import './main.css';
 
 import App from './components/App';
-import Hello from './components/Hello';
 import SignUpPage from './components/SignUpPage';
 import MessengerPage from './components/MessengerPage';
 
@@ -25,14 +28,17 @@ const theme = createMuiTheme({
   },
 });
 
+const store = createStore(reducers);
+
 render((
   <MuiThemeProvider theme={theme}>
-    <BrowserRouter>
-      <App>
-        <Route exact path="/" component={Hello} />
-        <Route path="/signup" component={SignUpPage} />
-        <Route path="/messenger" component={MessengerPage} />
-      </App>
-    </BrowserRouter>
+    <Provider store={store}>
+      <Router history={history}>
+        <App>
+          <Route exact path="/" component={MessengerPage} />
+          <Route path="/signup" component={SignUpPage} />
+        </App>
+      </Router>
+    </Provider>
   </MuiThemeProvider>
 ), document.getElementById('root'));
