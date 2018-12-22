76182e2bf90ccae68072d3c51731de437c6119dc
Artsiom Zhalkouski <azhalkouski@gmail.com>
2018-12-22 14:10:39 +0300

    Fetch chat messages

--- src/components/Chat/ChatContainer.jsx
+++ src/components/Chat/ChatContainer.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import ChatView from './ChatView';
+import { fetchChatMessages as fetchMessages } from './actions';
 
 class ChatContainer extends React.Component {
+  static propTypes = {
+    params: PropTypes.shape({
+      chatId: PropTypes.string.isRequired,
+    }).isRequired,
+    fetchChatMessages: PropTypes.func.isRequired,
+  }
+
+  componentDidMount() {
+    const { fetchChatMessages, params } = this.props;
+    fetchChatMessages(params.chatId);
+  }
+
   render() {
     return (
       <ChatView />
@@ -10,7 +24,10 @@ class ChatContainer extends React.Component {
   }
 }
 
-const mapStateToProps = {};
-const mapDispatchToProps = {};
+const mapStateToProps = null;
+
+const mapDispatchToProps = {
+  fetchChatMessages: fetchMessages,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(ChatContainer);
--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,10 +1,12 @@
 import { all } from 'redux-saga/effects';
 import auth from './components/Auth/sagas';
 import messenger from './components/MessengerPage/sagas';
+import chat from './components/Chat/sagas';
 
 export default function* () {
   yield all([
     auth(),
     messenger(),
+    chat(),
   ]);
 }
