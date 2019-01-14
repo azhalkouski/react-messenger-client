9c08c464a0868810712e33afb42c99228ee4e5a5
Artsiom Zhalkouski <azhalkouski@gmail.com>
2019-01-14 17:55:42 +0300

    Improve scroll

--- src/features/Chat/ChatView.jsx
+++ src/features/Chat/ChatView.jsx
@@ -1,43 +1,45 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import ReactDOM from 'react-dom';
 import PropTypes from 'prop-types';
-import moment from 'moment';
+import ChatMessages from './components/Messages';
 import ChatForm from './ChatForm';
 
-export default function ChatView(props) {
-  const { messages, peer, onMessagePost } = props;
+class ChatView extends PureComponent {
+  static propTypes = {
+    messages: PropTypes.array.isRequired,
+    peer: PropTypes.object.isRequired,
+    onMessagePost: PropTypes.func.isRequired,
+  };
 
-  return (
-    <div className="messenger-page__chat">
-      <div className="chat__header">
-        <div className="header__info">
-          <img className="info__photo" src={peer.photoUrl} />
-          <span className="info__name">{peer.fullName}</span>
-          <span className="info__status">{peer.status === 'online' ? 'Online' : 'Offline'}</span>
+  constructor(props) {
+    super(props);
+    this.messagesRef = React.createRef();
+  }
+
+  componentDidUpdate() {
+    const { scrollHeight } = this.messagesRef.current;
+    ReactDOM.findDOMNode(this.messagesRef.current).scrollTop = scrollHeight;
+  }
+
+  render() {
+    const { messages, peer, onMessagePost } = this.props;
+
+    return (
+      <div className="messenger-page__chat">
+        <div className="chat__header">
+          <div className="header__info">
+            <img className="info__photo" src={peer.photoUrl} />
+            <span className="info__name">{peer.fullName}</span>
+            <span className="info__status">{peer.status === 'online' ? 'Online' : 'Offline'}</span>
+          </div>
         </div>
+        <div className="chat-messages-wrapper" ref={this.messagesRef}>
+          <ChatMessages messages={messages} />
+        </div>
+        <ChatForm onSubmit={onMessagePost} />
       </div>
-      <div className="chat-messages">
-        {
-          messages.map(message => (
-            <div className="chat-messages__message" key={message._id}>
-              <img className="message__user-photo" src={message.user.photoUrl} alt="user" />
-              <div className="message__content">
-                <div className="message__content-header">
-                  <span className="message__sender">{message.user.fullName}</span>
-                  <span className="message__time">{moment(message.created).format('h:mm A')}</span>
-                </div>
-                <span className="message__content-body">{message.text}</span>
-              </div>
-            </div>
-          ))
-        }
-      </div>
-      <ChatForm onSubmit={onMessagePost} />
-    </div>
-  );
+    );
+  }
 }
 
-ChatView.propTypes = {
-  messages: PropTypes.array.isRequired,
-  peer: PropTypes.object.isRequired,
-  onMessagePost: PropTypes.func.isRequired,
-};
+export default ChatView;
